54a4117523696cf319b068ad6dbf8b5cb51efee5
mohandamged70m <mohandamged70m@icloud.com>
2026-05-08 01:03:24 +0300

    Update work experience to show only Kana Dojo contribution

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -28,83 +28,16 @@ const Experience = () => {
             className="md:flex md:items-center md:justify-between grid mt-8"
           >
             <span className="md:text-sm text-[13px] text-gray-500 px-2 mb-2">
-              2024-Present
+              2026 - Present
             </span>
 
             <div className="flex-center">
               <h1 className="text-gray-500 max-sm:hidden">
-                Founder and Developer{" "} at
+                Full Stack Developer at
               </h1>
               <Button className="bg-[#EEF4FF] text-[#3B82F6] ml-2">
                 <Star />
-                MideCode
-              </Button>
-            </div>
-          </motion.div>
-
-          <motion.div
-            initial={{ opacity: 0, x: -30 }}
-            animate={isInView ? { opacity: 1, x: 0 } : { opacity: 0, x: -30 }}
-            transition={{ duration: 0.6, delay: 0.4, ease: "easeOut" }}
-            className="md:flex md:items-center md:justify-between grid mt-8"
-          >
-            <span className="md:text-sm text-[13px] text-gray-500 px-2 mb-2">
-              2025-2026
-            </span>
-
-            <div className="flex-center">
-              <h1 className="text-gray-500 max-sm:hidden">
-                Frontend Developer at
-              </h1>
-              <Button className="bg-[#EEF4FF] text-[#3B82F6] ml-2">
-                <Star />
-                Bright <span className="text-yellow-600/90">Sparks Academy</span>
-              </Button>
-            </div>
-          </motion.div>
-
-          <motion.div
-            initial={{ opacity: 0, x: -30 }}
-            animate={isInView ? { opacity: 1, x: 0 } : { opacity: 0, x: -30 }}
-            transition={{ duration: 0.6, delay: 0.6, ease: "easeOut" }}
-            className="md:flex md:items-center md:justify-between grid mt-8"
-          >
-            <span className="md:text-sm text-[13px] text-gray-500 px-2 mb-2">
-              2025-Present
-            </span>
-
-            <div className="flex-center">
-              <h1 className="text-gray-500 max-sm:hidden">
-                Externship AI & Machine Learning at
-              </h1>
-              <Button className="bg-[#EEF4FF] text-[#3B82F6] ml-2">
-                <Star />
-                <span className="text-black">
-                  Ex<span className="text-green-800">tern</span>
-                </span>
-              </Button>
-            </div>
-          </motion.div>
-
-          <motion.div
-            initial={{ opacity: 0, x: -30 }}
-            animate={isInView ? { opacity: 1, x: 0 } : { opacity: 0, x: -30 }}
-            transition={{ duration: 0.6, delay: 0.6, ease: "easeOut" }}
-            className="md:flex md:items-center md:justify-between grid mt-8"
-          >
-            <span className="md:text-sm text-[13px] text-gray-500 px-2 mb-2">
-              2025-Present
-            </span>
-
-            <div className="flex-center">
-              <h1 className="text-gray-500 max-sm:hidden">
-              Backend Engineer at 
-              </h1>
-              <Button className="bg-[#EEF4FF] text-[#3B82F6] ml-2">
-                <Star />
-                <span className="text-black">
-                  Zend<span className="text-[#1ABC55]">solv</span>
-                </span>
+                Kana Dojo
               </Button>
             </div>
           </motion.div>
